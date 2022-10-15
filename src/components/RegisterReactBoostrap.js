58f60f7ed6f614d33a1e11bb046df576468ca54f
Sheikh sadi <you@example.com>
2022-10-15 21:34:05 +0600

    another commit

--- src/components/RegisterReactBoostrap.js
+++ src/components/RegisterReactBoostrap.js
@@ -1,13 +1,26 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase.init';
+
+const auth = getAuth(app);
 
 const RegisterReactBoostrap = () => {
      const handleRegister = event =>{
-          event.preventDefault();
+     event.preventDefault();
          const email = event.target.email.value;
          const password = event.target.password.value;
          console.log(email, password);
+         
+         createUserWithEmailAndPassword(auth, email, password)
+         .then(result => {
+          const user = result.user;
+          console.log(user);
+         })
+         .catch(error =>{
+          console.error('error', error);
+         })
      }
 
      return (
@@ -16,13 +29,13 @@ const RegisterReactBoostrap = () => {
                     <h3 className='text-primary'>Please register!!</h3>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                          <Form.Label>Email address</Form.Label>
-                         <Form.Control type="email" name='email' placeholder="Enter email" />
+                         <Form.Control type="email" name='email' placeholder="Enter email" required/>
                          
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                          <Form.Label>Password</Form.Label>
-                         <Form.Control type="password" name='password' placeholder="Password" />
+                         <Form.Control type="password" name='password' placeholder="Password" required/>
                     </Form.Group>
                     
                     <Button variant="primary" type="submit">Register</Button>
